783cea0f1718eb06a624f36acf351fcff6085772
kiragit01 <kiragithub@gmail.com>
2024-05-20 19:31:30 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateOOP1
+{
+    class Program
+    {
+        delegate double SearchDouble(double x, double y, double z);
+        static void Main()
+        {
+            SearchDouble searchDouble;
+            searchDouble = SmallDouble;
+
+            Console.Write("x = ");
+            int x = int.Parse(Console.ReadLine());
+            Console.Write("y = ");
+            int y = int.Parse(Console.ReadLine());
+            Console.Write("z = ");
+            int z = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Наименьшее = {searchDouble(x, y, z)}");
+
+            searchDouble = BigDouble;
+
+            Console.WriteLine($"Наибольшее = {searchDouble(x, y, z)}");
+
+            Console.ReadKey();
+        }
+
+        static double SmallDouble(double x, double y, double z)
+        {
+            if (x < y && x < z) return x;
+            if (y < z && y < x) return y;
+            if (z < x && z < y) return z;
+            return 0;
+        }
+
+        static double BigDouble(double x, double y, double z)
+        {
+            if (x > y && x > z) return x;
+            if (y > z && y > x) return y;
+            if (z > x && z > y) return z;
+            return 0;
+        }
+
+    }
+}
